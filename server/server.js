384b071eb60c362dd21c2811408d955d983f3482
David Pinhasik <davidpinhasik@gmail.com>
2018-06-07 17:38:31 +0300

    Add feolocation support via geolocation API plus new test

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const http     = require('http');
 const express  = require('express');
 const socketIO = require('socket.io');
 
-const {generateMessage} = require('./utils/message');
+const {generateMessage, generateLocationMessage} = require('./utils/message');
 const port = process.env.PORT || 3000;
 var app    = express();
 var server = http.createServer(app);
@@ -28,6 +28,11 @@ io.on('connection', (socket) => {
     //   text: message.text,
     //   createdAt: new Date().getTime()
     // })
+  });
+
+  socket.on('createLocationMessage', (coords, callback) => {
+    console.log('New location message arrived at server from client: ', coords);
+    io.emit('newLocationMessage', generateLocationMessage('Admin',  coords.latitude, coords.longitude));
   });
 
   socket.on('disconnect', () => {
--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,12 +1,21 @@
 var expect = require('expect');
 
-var {generateMessage} = require('./message');
+var {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generateMessage', () => {
   it('should generate correct message object', () => {
     var res = generateMessage('David', 'This a test message');
     expect(res.from).toBe('David');
     expect(res.text).toBe('This a test message');
+    expect(typeof res.createdAt).toBe('number');
+  });
+});
+
+describe('generateLocationMessage', () => {
+  it('should generate correct location object', () => {
+    var res = generateLocationMessage('David', 1, 2);
+    expect(res.from).toBe('David');
+    expect(res.url).toBe('https://www.google.com/maps?q=1,2');
     expect(typeof res.createdAt).toBe('number');
   });
 });
